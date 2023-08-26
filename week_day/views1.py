be3cf3e4c24713cb59298b831ab9a9009a3456a4
Javlonbek <sattorovjavlonbek6971@gmail.com>
2023-08-26 17:20:05 +0500

    1-home work django

--- week_day/views1.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-# Create your views here.
-def weekday1View(request):
-    return HttpResponse('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
